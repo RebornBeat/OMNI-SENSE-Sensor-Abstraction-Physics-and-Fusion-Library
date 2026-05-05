1e35ef58f420378d380eb011bb4d142ffced4dd7
RebornBeat <christian.lfonts@gmail.com>
2026-05-05 19:44:45 -0400

    Create mod.rs

---
+++ omni-sense-core/src/traits/mod.rs
@@ -0,0 +1,11 @@
+pub mod acoustic;
+pub mod binary;
+pub mod environmental;
+pub mod event_sensor;
+pub mod imu;
+pub mod ranging;
+pub mod sensor;
+pub mod velocity;
+
+#[cfg(feature = "camera")]
+pub mod imaging;
